b44ae4758ebc73a5bbb6d237d03734a0a3350686
Taha Ali <tahairfan1993@gmail.com>
2020-04-11 19:03:06 +0500

    removed moment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,6 @@ require("channels")
 import "bootstrap"
 import "jquery/dist/jquery"
 import "popper.js/dist/popper"
-import "moments"
 import "bootstrap-datetimepicker/src/js/bootstrap-datetimepicker"
 import "nouislider/distribute/nouislider.min"
 import "typed/src/typeCheck"
