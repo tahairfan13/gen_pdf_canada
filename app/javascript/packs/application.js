4d71cd0b8e38dfb3347df9f44830cd3c9fda8b04
Taha Ali <tahairfan1993@gmail.com>
2020-04-11 06:24:38 +0500

    integrated theme

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,11 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import "bootstrap"
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+import "jquery/dist/jquery"
+import "popper.js/dist/popper"
+import "moments"
+import "bootstrap-datetimepicker/src/js/bootstrap-datetimepicker"
+import "nouislider/distribute/nouislider.min"
+import "typed/src/typeCheck"
